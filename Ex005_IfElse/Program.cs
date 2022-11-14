786d0d68d03e9ffb93d56cf4e980d53a202f2936
Natali <vrode.nata@mail.ru>
2022-11-14 17:31:46 +0300

    add Ex 005 If-Else

---
+++ Ex005_IfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Hooray, it's MASHA!");
+
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
